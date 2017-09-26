4b84acbff3f6c4cac9400eb739e24a0b7a5f27f4
SKvasnytsia <soli4ka16@gmail.com>
2017-09-26 17:20:12 +0300

    updates to styles

--- src/service-worker.js
+++ src/service-worker.js
@@ -11,30 +11,30 @@ const cacheList = [
   'index.html',
   'https://fonts.googleapis.com/icon?family=Material+Icons'
 ]
-const getStartAndEndDatesPerMonth = function(date) {
-    const year = date.getFullYear(),
-        month = date.getMonth()
+const getStartAndEndDatesPerMonth = function (date) {
+  const year = date.getFullYear(),
+    month = date.getMonth()
 
-    return { 
-        from : new Date(year, month, 1), 
-        to: new Date(year, month + 1, 0) 
-    }
+  return {
+    from: new Date(year, month, 1),
+    to: new Date(year, month + 1, 0)
+  }
 }
 
-const separateToMonthlyRanges = function(from, to, isOriginRange = true) {
+const separateToMonthlyRanges = function (from, to, isOriginRange = true) {
   const _getRange = (start, finish, currentYear) => {
-  const array = []
+    const array = []
 
-  for (let i = start; i <= finish; i++) {
-      let {from: f, to: t} = 
-      this.getStartAndEndDatesPerMonth(new Date(fromYear, i))
+    for (let i = start; i <= finish; i++) {
+      let { from: f, to: t } =
+        this.getStartAndEndDatesPerMonth(new Date(fromYear, i))
       if (isOriginRange) {
-      if (i === start && currentYear === fromYear) f = from
-      if (i === finish && currentYear === toYear) t = to
+        if (i === start && currentYear === fromYear) f = from
+        if (i === finish && currentYear === toYear) t = to
       }
-      array.push({from: f, to: t})
-  }
-  return array
+      array.push({ from: f, to: t })
+    }
+    return array
   }
 
   let objRange = []
@@ -43,83 +43,86 @@ const separateToMonthlyRanges = function(from, to, isOriginRange = true) {
   let toMonth = to.getMonth()
   let toYear = to.getFullYear()
 
-  if (fromMonth === toMonth && fromYear === toYear) 
-  return [{ from, to }]
-  
-  if (fromYear === toYear && fromMonth < toMonth){
-  return _getRange(fromMonth, toMonth, fromYear)
+  if (fromMonth === toMonth && fromYear === toYear)
+    return [{ from, to }]
+
+  if (fromYear === toYear && fromMonth < toMonth) {
+    return _getRange(fromMonth, toMonth, fromYear)
   }
 
   if (fromYear < toYear) {
-  for (let j = fromYear; j <= toYear; j++) {
+    for (let j = fromYear; j <= toYear; j++) {
       let i = fromMonth
       let monthlyEnd = 12
       if (j > fromYear) i = 1
       if (j === toYear) monthlyEnd = toMonth
       objRange = objRange.concat(_getRange(i, monthlyEnd, j))
+    }
+    return objRange
   }
-  return objRange
-  }
-  
+
 }
 
-const authenticatedRequests = function() {
-  firebase.prototype.auth.currentUser.getToken(true).then(token =>{
+const authenticatedRequests = function () {
+  firebase.prototype.auth.currentUser.getToken(true).then(token => {
     console.log(token)
   })
 }
 
-self.addEventListener('beforeinstallprompt', function(event) {
-   event.userChoice.then(function(choiceResult) {
-   if(choiceResult.outcome == 'dismissed') {
+self.addEventListener('beforeinstallprompt', function (event) {
+  event.userChoice.then(function (choiceResult) {
+    if (choiceResult.outcome == 'dismissed') {
       console.log('User cancelled home screen install');
     }
     else {
       console.log('User added to home screen');
-   }
+    }
   });
 });
 
-self.addEventListener('install', function(event) {
+self.addEventListener('install', function (event) {
   var offlineRequest = new Request('offline.html');
   event.waitUntil(
-    fetch(offlineRequest).then(function(response) {
-      return caches.open('offline').then(function(cache) {
+    fetch(offlineRequest).then(function (response) {
+      return caches.open('offline').then(function (cache) {
         return cache.put(offlineRequest, response);
       });
     })
   );
 });
 
-self.addEventListener('activate', function(event) {
+self.addEventListener('activate', function (event) {
   console.log('activate')
   //authenticatedRequests()
 
 });
 
-self.addEventListener('fetch', function(event) {
-  
+self.addEventListener('fetch', function (event) {
+
   console.log('fetch', event.request)
   if (event.request.method === 'GET') {
-    event.respondWith(caches.match(event.request).then(function(response) {
-      // caches.match() always resolves
-      // but in case of success response will have value
-      if (response) {
-        return response;
-      } else {
-        return fetch(event.request).then(function (response) {
-          if(!response || response.status !== 200 || response.type !== 'basic'){
-              return response
-          }
-          let responseClone = response.clone();
-          
-          caches.open('v1').then(function (cache) {
-            cache.put(event.request, responseClone);
-          });
-          return response;
-        }).catch(function () {
-        });
-      }
-    }));
+    event.respondWith(
+      fetch(event.request).catch(function () {
+        console.log('Network failed so trying to get from cache: ' + event.request.url);
+
+        return caches.open(CACHE_NAME)
+          .then(function (cache) {
+            return cache.match(event.request)
+              .then(function (response) {
+                if (response) {
+                  console.log('Cache for : ' + event.request.url);
+                  return response;
+                }
+                return caches.match('offline.html').then(response => {
+                  if (response) {
+                    console.log('offline found');
+                    return response;
+                  }
+                  console.log('no offline file found')
+                })
+              })
+          })
+      })
+    )
   }
-});
+})
